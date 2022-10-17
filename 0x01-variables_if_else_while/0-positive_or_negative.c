d8d4305f4289e87eefacc59114d42ab54f9eec02
Okpako Evi <eviokpako.c@gmail.com>
2022-10-16 21:56:57 -0700

    my work

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,9 +3,9 @@
 #include <stdio.h>
 
 /**
- * main - print a random number
+ * main - print a random num
  *
- * Reture: 0
+ * Return: 0
  */
 int main(void)
 {
@@ -18,6 +18,6 @@ int main(void)
 	else if (n < 0)
 		printf("%d is negative\n", n);
 	else
-		printf("%d is zero\n", n);
+		print("%d is zero\m", n);
 	return (0);
 }
