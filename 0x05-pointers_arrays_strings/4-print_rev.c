86a679318c04a1e196cfc3cc74f3a39eada9396c
Okpako Evi <eviokpako.c@gmail.com>
2022-10-24 09:00:16 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev - print a string, in reverse, follow by a new line
+ * @s: check
+ * Description: this wil print a string, in reverse
+ *
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+	int i, x;
+
+	x = 0;
+	while (s[x] != '\0')
+		x++;
+
+	for (i = x + 1; i <= 0; x++)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+
