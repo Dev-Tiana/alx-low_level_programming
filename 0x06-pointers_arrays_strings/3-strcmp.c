d6d73513ad813b0f5c080c702dae44259e033f19
Okpako Evi <eviokpako.c@gmail.com>
2022-10-27 11:23:41 -0700

    arrays

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strcmp - compare two string
+ * @s1:stamp
+ * @s2:stran
+ *
+ * Return: 0
+ *
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (((*s1 != '\0') && (*s2 != '\0')) && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+	}
+
+	if (*s1 == *s2)
+	{
+		return (0);
+	}
+
+	else
+	{
+		return (*s1 - *s2);
+	}
+}
