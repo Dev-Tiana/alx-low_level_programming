7c1a8475d799746d63ac0156fe07e36200827d2e
Dev-Tiana <eviokpako.c@gmail.com>
2023-11-15 04:17:57 -0800

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,24 +1,16 @@
-#include <stdio.h>
-
-void _puts_recursion(char *s) {
-    if (*s == '\0') {
-        // Base case: end of string
-        printf("\n");
-        return;
-    }
-
-    // Print the current character
-    printf("%c", *s);
-
-    // Make a recursive call with the rest of the string
-    _puts_recursion(s + 1);
+#include "main.h"
+/**
+ * _puts_recursion - prints a string, followed by a new line.
+ * @s: string
+ * Return: no return.
+ */
+void _puts_recursion(char *s)
+{
+if (*s != '\0')
+{
+_putchar(*s);
+_puts_recursion(s + 1);
 }
-
-int main() {
-    char secretMessage[] = "She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget";
-    
-    // Call the recursive function to print the string
-    _puts_recursion(secretMessage);
-
-    return 0;
+else
+_putchar('\n');
 }
