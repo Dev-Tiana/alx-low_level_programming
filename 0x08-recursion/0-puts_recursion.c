dc34f50c3fd0925e469689005c854704b24b45ed
Dev-Tiana <eviokpako.c@gmail.com>
2023-11-15 03:32:13 -0800

    recu

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,20 +1,24 @@
 #include <stdio.h>
-#include "main.h"
-/**
- * scretmessage - prints a secret message.
- */
-void secretmessage(void)
-{
-         printf("she locked away a secret, deep inside herself,\n);
-	 printf("something she once knew to be true... but chose to forget\n");
+
+void _puts_recursion(char *s) {
+    if (*s == '\0') {
+        // Base case: end of string
+        printf("\n");
+        return;
+    }
+
+    // Print the current character
+    printf("%c", *s);
+
+    // Make a recursive call with the rest of the string
+    _puts_recursion(s + 1);
 }
-/**
- * main - Entry point of the program.
- *
- * Return: 0 on suceess.
- */
-int main(void)
-{
-	secretmessage(); // calling the function
-	return 0;
+
+int main() {
+    char secretMessage[] = "She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget";
+    
+    // Call the recursive function to print the string
+    _puts_recursion(secretMessage);
+
+    return 0;
 }
